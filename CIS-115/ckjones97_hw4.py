f9b0c6bb6407e81cf3dcaaeab31b99c0ed268233
ColtonJones197 <ckjones97@gmail.com>
2022-07-19 11:16:02 -0500

    completed cis115 hw 4

--- CIS-115/ckjones97_hw4.py
+++ CIS-115/ckjones97_hw4.py
@@ -1,4 +1,7 @@
-# Gets a number between 1 and 3 from the user, showing how many sticks they'd like to grab
+# Author: Colton Jones
+# Section: ZA
+# Description: Simulates STICK GAME in the console
+from random import random, randrange
 
 # takes in the current player and the number of sticks currently on the board
 # returns the number of sticks taken by the player, which needs to be grabbed as an input.
@@ -34,9 +37,38 @@ def display_board(sticks_on_board: int) -> None:
 def display_summary(current_player_number: int, sticks_taken: int, sticks_remaining: int) -> None:
     print(f'Player {current_player_number} took {sticks_taken}! There are {sticks_remaining} sticks remaining.')
 
+#control flow
 def main():
-    print('main')
+    # initialize script
+    should_continue_game = True
+    starting_sticks, sticks_available = 20, 20
+    players = [1, 2]
+    turn_index = players[-1] # starts at the end for while loop flow, so that the correct winner is easy to determine
+    magic_odds = 0.15 # percent chance that 1-4 sticks are randomly added back to the board
+
+    print('WELCOME TO THE STICK GAME!')
+    while should_continue_game:
+        print('GAME START')
+        # go through a turn until the end is reached
+        while sticks_available > 0:
+            # determine turn
+            turn_index = turn_index + 1 if turn_index < len(players) - 1 else 0
+            display_board(sticks_available)
+            taken = get_taken_sticks(players[turn_index], sticks_available)
+            sticks_available -= taken
+            display_summary(players[turn_index], taken, sticks_available)
+            # see if sticks should be added back to the board
+            roll = random()
+            if roll <= magic_odds:
+                # figure out how many sticks to add back
+                # needs the min because we never want to go above the number of starting sticks
+                num_added_back = randrange(1, min(4, starting_sticks - sticks_available))
+                sticks_available += num_added_back
+                print(f'Whoa! {num_added_back} sticks magically reappear from the aether!\n{sticks_available} sticks are left now!')
+        # if the loop exits, the player who's current turn it is loses
+        print(f'Player {players[turn_index]} loses!')
+        exit_input = input('Thanks for playing STICK GAME! Enter X to exit, otherwise the game will restart: ')
+        should_continue_game = False if exit_input.upper() == 'X' else True
 
 if __name__ == '__main__':
-    display_board(12)
-    input()
+    main()
